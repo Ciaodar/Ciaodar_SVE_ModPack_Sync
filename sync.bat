3fb24e5bd69d61e8809b97f0832d1801668230f9
Metehan <gliencherth@gmail.com>
2024-09-19 01:42:12 +0300

    AutoSync

--- sync.bat
+++ sync.bat
@@ -1,11 +1,7 @@
 @echo off
-
 cd "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley\Mods"
-
 git fetch
 git pull
-git add .
-git commit -m "AutoSync"
-git push origin master
-echo Finished
+
+echo Syncronisation Finished
 
